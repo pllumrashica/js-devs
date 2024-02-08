60b3e66838e2f2165aaa3224b24888fa6c87404e
Pellumb Rashica <103010130+pllumrashica@users.noreply.github.com>
2024-02-08 20:10:16 +0100

    arrays examples

---
+++ JS - Class/Arrays/example-one.js
@@ -0,0 +1,35 @@
+let students = [
+  {
+    id: 0,
+    firstName: "Pellumb",
+    lastName: "Rashica",
+    age: 19,
+  },
+  {
+    id: 1,
+    firstName: "Amar",
+    lastName: "Mejzini",
+    age: 18,
+  },
+];
+
+document.write(
+  `<table><th>ID</th><th>First Name</th> <th>Last Name</th> <th>Age</th>`
+);
+
+students.forEach((student) => {
+  document.write(
+    `
+        <tbody>
+            <tr>
+            <td>${student.id}</td>
+            <td>${student.firstName}</td>
+            <td>${student.lastName}</td>
+            <td>${student.age}</td>
+            </tr>
+        </tbody>
+        `
+  );
+});
+
+document.write(`</table>`);
--- JS - Homework/homework-4/script.js
+++ JS - Homework/homework-4/script.js
@@ -3,170 +3,133 @@
 let products = [
   {
     id: 0,
-    img:
-      "<div class='product-img-box'>" +
-      "<img class='product-img' src='./imgs/monitor-benq-zowie.png'>" +
-      "</div>",
-    title:
-      "<h1 class='main-title'>" +
-      'Monitor BenQ ZOWIE XL2540K, 24.5", 240Hz, i zi' +
-      "</h1>",
-    mainPrice: "<h2 class='main-price'>" + 299.5 + "€" + "</h2>",
-    oldPrice: "<h2 class='old-price'>" + 359.5 + "€" + "</h2>",
-    tvsh: "<span class='tvsh'>" + "Përfshirë TVSH" + "</span>",
+    img: "./imgs/monitor-benq-zowie.png",
+    title: 'Monitor BenQ ZOWIE XL2540K, 24.5", 240Hz, i zi',
+    mainPrice: 299.5 + "€",
+    oldPrice: 359.5 + "€",
+    tvsh: "Përfshirë TVSH",
+    btns: "Shto ne shport",
   },
   {
     id: 1,
-    img:
-      "<div class='product-img-box'>" +
-      "<img class='product-img' src='./imgs/fenix-series-23.png'>" +
-      "</div>",
+    img: "./imgs/fenix-series-23.png",
     title:
-      "<h1 class='main-title'>" +
-      "Kompjuter Gjirafa50 FenixSeries 23, Intel Core i5-14400F, 16GB RAM, 500GB SSD, ASUS Dual RTX 4060 OC 8GB, i zi" +
-      "</h1>",
-    mainPrice: "<h2 class='main-price'>" + 889.5 + "€" + "</h2>",
-    oldPrice: "<h2 class='old-price'>" + "1,172.15" + "€" + "</h2>",
-    tvsh: "<span class='tvsh'>" + "Përfshirë TVSH" + "</span>",
+      "Kompjuter Gjirafa50 FenixSeries 23, Intel Core i5-14400F, 16GB RAM, 500GB SSD, ASUS Dual RTX 4060 OC 8GB, i zi",
+    mainPrice: 889.5 + "€",
+    oldPrice: "1,172.15" + "€",
+    tvsh: "Përfshirë TVSH",
+    btns: "Shto ne shport",
   },
   {
     id: 2,
-    img:
-      "<div class='product-img-box'>" +
-      "<img class='product-img' src='./imgs/samsung-galaxy-s24-ultra.png'>" +
-      "</div>",
+    img: "./imgs/samsung-galaxy-s24-ultra.png",
     title:
-      "<h1 class='main-title'>" +
-      'Celular Samsung Galaxy S24 Ultra, 6.8", 12GB/ 512GB, Titanium Black' +
-      "</h1>",
-    mainPrice: "<h2 class='main-price'>" + "1,599.50" + "€" + "</h2>",
-    oldPrice: "<h2 class='old-price no-discount'>" + "" + "€" + "</h2>",
-    tvsh: "<span class='tvsh'>" + "Përfshirë TVSH" + "</span>",
+      "Celular Samsung Galaxy S24 Ultra, 6.8'', 12GB/ 512GB, Titanium Black",
+    mainPrice: "1,599.50" + "€",
+    oldPrice: "1,599.50" + "€",
+    tvsh: "Përfshirë TVSH",
+    btns: "Shto ne shport",
   },
   {
     id: 3,
-    img:
-      "<div class='product-img-box'>" +
-      "<img class='product-img' src='./imgs/apple-macbook-pro-14inq.png'>" +
-      "</div>",
+    img: "./imgs/apple-macbook-pro-14inq.png",
     title:
-      "<h1 class='main-title'>" +
-      'Apple Macbook Pro 14", M3 Pro 11-Core, 18GB, 512GB, 14-Core GPU, Space Black' +
-      "</h1>",
-    mainPrice: "<h2 class='main-price'>" + "2,299.50" + "€" + "</h2>",
-    oldPrice: "<h2 class='old-price'>" + "2,499.50" + "€" + "</h2>",
-    tvsh: "<span class='tvsh'>" + "Përfshirë TVSH" + "</span>",
+      "Celular Samsung Galaxy S24 Ultra, 6.8'', 12GB/ 512GB, Titanium Black",
+    mainPrice: "2,299.50" + "€",
+    oldPrice: "2,499.50" + "€",
+    tvsh: "Përfshirë TVSH",
+    btns: "Shto ne shport",
   },
   {
     id: 4,
-    img:
-      "<div class='product-img-box'>" +
-      "<img class='product-img' src='./imgs/qant-shpine-lenovo.png'>" +
-      "</div>",
-    title:
-      "<h1 class='main-title'>" +
-      'Çantë shpine Lenovo B210 për laptop, 15.6"' +
-      "</h1>",
-    mainPrice: "<h2 class='main-price'>" + "14.50" + "€" + "</h2>",
-    oldPrice: "<h2 class='old-price'>" + "19.50" + "€" + "</h2>",
-    tvsh: "<span class='tvsh'>" + "Përfshirë TVSH" + "</span>",
+    img: "./imgs/qant-shpine-lenovo.png",
+    title: "Çantë shpine Lenovo B210 për laptop, 15.6''",
+    mainPrice: "14.50" + "€",
+    oldPrice: "19.50" + "€",
+    tvsh: "Përfshirë TVSH",
+    btns: "Shto ne shport",
   },
   {
     id: 5,
-    img:
-      "<div class='product-img-box'>" +
-      "<img class='product-img' src='./imgs/fenix-series-61.png'>" +
-      "</div>",
+    img: "./imgs/fenix-series-61.png",
     title:
-      "<h1 class='main-title'>" +
-      "Kompjuter Gjirafa50 Fenix Series 61, Intel Core i7-14700F, 32GB RAM DDR5, 1TB SSD M.2, MSI RTX 4070 SUPER VENTUS 2X OC 12GB , i zi" +
-      "</h1>",
-    mainPrice: "<h2 class='main-price'>" + "1,669.50" + "€" + "</h2>",
-    oldPrice: "<h2 class='old-price'>" + "2,062.22" + "€" + "</h2>",
-    tvsh: "<span class='tvsh'>" + "Përfshirë TVSH" + "</span>",
+      "Kompjuter Gjirafa50 Fenix Series 61, Intel Core i7-14700F, 32GB RAM DDR5, 1TB SSD M.2, MSI RTX 4070 SUPER VENTUS 2X OC 12GB , i zi",
+    mainPrice: "1,669.50" + "€",
+    oldPrice: "2,062.22" + "€",
+    tvsh: "Përfshirë TVSH",
+    btns: "Shto ne shport",
   },
   {
     id: 6,
-    img:
-      "<div class='product-img-box'>" +
-      "<img class='product-img' src='./imgs/processor-intel-i5.png'>" +
-      "</div>",
-    title:
-      "<h1 class='main-title'>" + "Procesor Intel Core i5-14600KF" + "</h1>",
-    mainPrice: "<h2 class='main-price'>" + "399.50" + "€" + "</h2>",
-    oldPrice: "<h2 class='old-price'>" + "416.50" + "€" + "</h2>",
-    tvsh: "<span class='tvsh'>" + "Përfshirë TVSH" + "</span>",
+    img: "./imgs/processor-intel-i5.png",
+    title: "Procesor Intel Core i5-14600KF",
+    mainPrice: "399.50" + "€",
+    oldPrice: "416.50" + "€",
+    tvsh: "Përfshirë TVSH",
+    btns: "Shto ne shport",
   },
   {
     id: 7,
-    img:
-      "<div class='product-img-box'>" +
-      "<img class='product-img' src='./imgs/degjuse-dudao-u10b.png'>" +
-      "</div>",
-    title:
-      "<h1 class='main-title'>" + "Dëgjuese Dudao U10B, të bardha" + "</h1>",
-    mainPrice: "<h2 class='main-price'>" + "11.40" + "€" + "</h2>",
-    oldPrice: "<h2 class='old-price'>" + "17.00" + "€" + "</h2>",
-    tvsh: "<span class='tvsh'>" + "Përfshirë TVSH" + "</span>",
+    img: "./imgs/degjuse-dudao-u10b.png",
+    title: "Dëgjuese Dudao U10B, të bardha",
+    mainPrice: "11.40" + "€",
+    oldPrice: "17.00" + "€",
+    tvsh: "Përfshirë TVSH",
+    btns: "Shto ne shport",
   },
   {
     id: 8,
-    img:
-      "<div class='product-img-box'>" +
-      "<img class='product-img' src='./imgs/usb-kingston.png'>" +
-      "</div>",
-    title:
-      "<h1 class='main-title'>" +
-      "USB Kingston Data Traveler Exodia, 128GB, e zezë" +
-      "</h1>",
-    mainPrice: "<h2 class='main-price'>" + "8.50" + "€" + "</h2>",
-    oldPrice: "<h2 class='old-price'>" + "13.50" + "€" + "</h2>",
-    tvsh: "<span class='tvsh'>" + "Përfshirë TVSH" + "</span>",
+    img: "./imgs/usb-kingston.png",
+    title: "USB Kingston Data Traveler Exodia, 128GB, e zezë",
+    mainPrice: "8.50" + "€",
+    oldPrice: "13.50" + "€",
+    tvsh: "Përfshirë TVSH",
+    btns: "Shto ne shport",
   },
   {
     id: 9,
-    img:
-      "<div class='product-img-box'>" +
-      "<img class='product-img' src='./imgs/altropalant-gembird.png'>" +
-      "</div>",
-    title:
-      "<h1 class='main-title'>" +
-      "Altoparlant Gembird SPK-BT-11 2.1, i zi" +
-      "</h1>",
-    mainPrice: "<h2 class='main-price'>" + "17.50" + "€" + "</h2>",
-    oldPrice: "<h2 class='old-price no-discount'>" + "" + "€" + "</h2>",
-    tvsh: "<span class='tvsh'>" + "Përfshirë TVSH" + "</span>",
+    img: "./imgs/altropalant-gembird.png",
+    title: "Altoparlant Gembird SPK-BT-11 2.1, i zi",
+    mainPrice: "17.50" + "€",
+    oldPrice: "" + "€",
+    tvsh: "Përfshirë TVSH",
+    btns: "Shto ne shport",
   },
 ];
 
-let productsHTML = ""; // Store HTML for all products
-let productsInRow = 5; // Number of products to display in each row
+let productsHTML = ""; // an empty string for products to put in HTML
+let productsInRow = 5; // Number of products for each row
 
-// Iterate through each product
 products.forEach((product, index) => {
-  // Generate HTML for the product
   let productHTML = `
     <div class='col'>
-      ${product.img}
-      ${product.title}
-      ${product.mainPrice}
-      ${product.oldPrice}
-      <p>${product.tvsh}</p>
+    <div class='product-img-box'>
+      <img class='product-img' src='${product.img}'>
+    </div>
+      <h1 class="main-title">${product.title}</h1>
+      <h2 class="main-price">${product.mainPrice}</h2>
+      <h2 class="old-price">${product.oldPrice}</h2>
+      <span class='tvsh'>${product.tvsh}</span>
+      <div class='btns'>
+      <div class='cart-btn'>
+      <h2 class='btn-text'>
+      ${product.btns}
+      </h2>
+      </div>
+      <div class='wishlist-btn'>
+      <i class='fa-regular fa-heart'></i>
+      </div>
+      </div>
     </div>
   `;
 
-  // Add product HTML to the productsHTML string
   productsHTML += productHTML;
 
-  // Check if it's the last product in the row or the last product overall
   if ((index + 1) % productsInRow === 0 || index === products.length - 1) {
-    // If so, wrap the products in a row
     productsHTML = "<div class='row'>" + productsHTML + "</div>";
 
-    // Append the row to the document
     document.write(productsHTML);
 
-    // Reset productsHTML for the next row
     productsHTML = "";
   }
 });
