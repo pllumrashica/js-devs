d4aed0d61a652248d2ba007e4c10a92ef90c4333
Pellumb Rashica <103010130+pllumrashica@users.noreply.github.com>
2024-04-20 08:22:09 +0200

    homework 14 added

--- JS - Homework/homework-14/script.js
+++ JS - Homework/homework-14/script.js
@@ -8,6 +8,9 @@ let genderInput = document.querySelectorAll(
 let languageInput = document.querySelectorAll(
   "input[type='checkbox'][name='language']"
 );
+let yourMessageInput = document.getElementById("yourMessage");
+let emailInput = document.getElementById("emailInput");
+let passwordInput = document.getElementById("passwordInput");
 
 let firstNameHeading = document.getElementById("firstNameHeading");
 let lastNameHeading = document.getElementById("lastNameHeading");
@@ -16,6 +19,9 @@ let birthdayHeading = document.getElementById("birthdayHeading");
 let genderHeading = document.getElementById("genderHeading");
 let languageHeading = document.getElementById("languageHeading");
 let languageSecondHeading = document.getElementById("languageSecondHeading");
+let messageHeading = document.getElementById("messageHeading");
+let emailHeading = document.getElementById("emailHeading");
+let passwordHeading = document.getElementById("passwordHeading");
 
 const firstNameInputValue = (e) => {
   console.log(e.target.value);
@@ -43,8 +49,6 @@ genderInput.forEach((gender) => {
   });
 });
 
-
-
 languageInput.forEach((language) => {
   language.addEventListener("change", (e) => {
     console.log(e.target);
@@ -57,7 +61,25 @@ languageInput.forEach((language) => {
   });
 });
 
+const yourMessageInputValue = (e) => {
+  console.log(e.target.value);
+  messageHeading.innerText = e.target.value;
+};
+
+const emailInputValue = (e) => {
+  console.log(e.target.value);
+  emailHeading.innerText = e.target.value;
+};
+
+const passwordInputValue = (e) => {
+  console.log(e.target.value);
+  passwordHeading.innerText = e.target.value;
+};
+
 firstNameInput.addEventListener("input", firstNameInputValue);
 lastNameInput.addEventListener("input", lastNameInputValue);
 ageInput.addEventListener("input", ageInputValue);
 birthInput.addEventListener("input", birthInputValue);
+yourMessageInput.addEventListener("input", yourMessageInputValue);
+emailInput.addEventListener("input", emailInputValue);
+passwordInput.addEventListener("input", passwordInputValue);
