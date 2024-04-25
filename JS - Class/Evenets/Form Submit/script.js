e9fd49cd8c918ca297701ba159aa9990f202e213
Pellumb Rashica <103010130+pllumrashica@users.noreply.github.com>
2024-04-25 20:15:14 +0200

    input validation

--- JS - Class/Evenets/Form Submit/script.js
+++ JS - Class/Evenets/Form Submit/script.js
@@ -12,7 +12,45 @@ const onSubmit = (e) => {
   formData.forEach(function (value, key) {
     formObject[key] = value;
   });
+  clearError();
+
+  validateForm();
   console.log(formObject);
+};
+
+const validateForm = () => {
+  let isValid = true;
+  let nameValue = firstNameInput.value;
+  let lastNameValue = lastNameInput.value;
+  let emailValue = emailInput.value;
+
+  if (!nameValue.trim() || nameValue.length < 2) {
+    let nameError = document.getElementById("nameError");
+    nameError.innerText = "name should be more than two length";
+  }
+
+  if (
+    !lastNameValue.trim() ||
+    lastNameValue.length < 2 ||
+    lastNameValue.length > 6
+  ) {
+    let lastNameError = document.getElementById("lastNameError");
+    lastNameError.innerText = "name should be more than two and less than 6";
+    isValid = false;
+  }
+
+  if (!emailValue.includes("@")) {
+    let emailError = document.getElementById("emailError");
+    emailError.innerText = "email should have an @";
+    isValid = false;
+  }
+  return isValid;
+};
+
+const clearError = () => {
+  nameError.document.getElementById("nameError").innerText = "";
+  lastNameError.document.getElementById("lastNameError").innerText = "";
+  emailError.document.getElementById("emailError").innerText = "";
 };
 
 form.addEventListener("submit", onSubmit);
