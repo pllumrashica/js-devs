fcbd6d70e37ccae5d2e0392100b3e9a27adc55c8
Pellumb Rashica <103010130+pllumrashica@users.noreply.github.com>
2024-05-04 00:41:42 +0200

    homework 16

--- JS - Class/JS-CRUD/script.js
+++ JS - Class/JS-CRUD/script.js
@@ -1,76 +1,71 @@
-let form = document.getElementById("addStudentForm");
-let createBtn = document.getElementById("createBtn");
-// localStorage.clear();
+const studentForm = document.getElementById("addS");
+const studentTable = document.getElementById("studentTable");
+localStorage.clear();
 
-createBtn.addEventListener("click", function (e) {
-  e.preventDefault();
-  let studentFirstName = document.getElementById("studentFirstName").value;
-  let studentLastName = document.getElementById("studentLastName").value;
-  let studentAge = document.getElementById("studentAge").value;
-  let studentGender = document.getElementById("studentGender").value;
-  let studentBirthday = document.getElementById("studentBirthday").value;
-  let studentClass = document.getElementById("studentClass").value;
+const loadStudents = () => {
+  let students = JSON.parse(localStorage.getItem("students")) || [];
+  students.forEach((student, index) => {
+    let tr = document.createElement("tr");
+    studentTable.appendChild(tr);
 
-  let student = {
-    firstName: studentFirstName,
-    lastName: studentLastName,
-    age: studentAge,
-    gender: studentGender,
-    birthday: studentBirthday,
-    class: studentClass,
-  };
+    // Assign ID number to each user
+    let id = document.createElement("td");
+    id.innerHTML = index + 1;
+    tr.appendChild(id);
+
+    Object.values(student).forEach((value) => {
+      let td = document.createElement("td");
+      td.innerHTML = value;
+      tr.appendChild(td);
+    });
+
+    let actionsBtn = document.createElement("td");
+    let deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = "Delete";
+    deleteBtn.onclick = function () {
+      tr.remove();
+      students.splice(index, 1);
+      localStorage.setItem("students", JSON.stringify(students));
+    };
+    actionsBtn.appendChild(deleteBtn);
+    let editBtn = document.createElement("button");
+    editBtn.innerHTML = "Edit";
+    editBtn.onclick = function () {
+      // document.getElementById("id").value = tr.cells[0].innerHTML;
+      document.getElementById("name").value = tr.cells[1].innerHTML;
+      document.getElementById("surname").value = tr.cells[2].innerHTML;
+      document.getElementById("age").value = tr.cells[3].innerHTML;
+      document.getElementById("gender").value = tr.cells[4].innerHTML;
+      document.getElementById("birthDay").value = tr.cells[5].innerHTML;
+      document.getElementById("group").value = tr.cells[6].innerHTML;
+
+      studentButton.innerHTML = "Edit";
+      // Remove the existing student data from localStorage
+      students.splice(index, 1);
+      localStorage.setItem("students", JSON.stringify(students));
+    };
+    actionsBtn.appendChild(editBtn);
+    tr.appendChild(actionsBtn);
+  });
+};
+
+window.addEventListener("load", () => {
+  loadStudents();
+});
+
+const onStudentSubmit = (event) => {
+  event.preventDefault();
+  const formDataSecond = new FormData(document.getElementById("addS"));
+  const formObjectSecond = {};
+  formDataSecond.forEach(function (value, key) {
+    formObjectSecond[key] = value;
+  });
 
   let students = JSON.parse(localStorage.getItem("students")) || [];
-  students.push(student);
+  students.push(formObjectSecond);
   localStorage.setItem("students", JSON.stringify(students));
 
-  form.reset();
+  location.reload(); // Reload the page to reflect the changes
+};
 
-  // Get the tbody element
-  let tbody = document.querySelector("tbody");
-
-  // Clear existing rows
-  tbody.innerHTML = "";
-
-  // Loop through the students and create table rows
-  students.forEach(function (student) {
-    let row = document.createElement("tr");
-    row.innerHTML = `
-                        <td>${student.firstName}</td>
-                        <td>${student.lastName}</td>
-                        <td>${student.age}</td>
-                        <td>${student.gender}</td>
-                        <td>${student.birthday}</td>
-                        <td>${student.class}</td>
-                        <td>${student.actions}</td>
-
-                `;
-    tbody.appendChild(row);
-  });
-});
-
-let students = JSON.parse(localStorage.getItem("students")) || [];
-students.push(student);
-localStorage.setItem("students", JSON.stringify(students));
-
-form.reset();
-
-// Get the tbody element
-let tbody = document.querySelector("tbody");
-
-// Clear existing rows
-tbody.innerHTML = "";
-
-// Loop through the students and create table rows
-students.forEach(function (student) {
-  let row = document.createElement("tr");
-  row.innerHTML = `
-            <td>${student.firstName}</td>
-            <td>${student.lastName}</td>
-            <td>${student.age}</td>
-            <td>${student.gender}</td>
-            <td>${student.birthday}</td>
-            <td>${student.class}</td>
-        `;
-  tbody.appendChild(row);
-});
+studentForm.addEventListener("submit", onStudentSubmit);
